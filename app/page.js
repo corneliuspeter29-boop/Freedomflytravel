4358a9d83ace86fa0ab4a046938a624f5dae376c
corneliuspeter29-boop <corneliuspeter29@gmail.com>
2025-11-16 07:41:37 +0400

    Delete app directory

--- app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: "40px", fontFamily: "Arial" }}>
-      <h1>Freedom Fly Travel</h1>
-      <p>Your trusted travel partner for Dubai, Turkey & worldwide adventures.</p>
-    </main>
-  );
-}
